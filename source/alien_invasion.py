eec77d7a2fc85cf86590df94cf744d8e6580ac7c
seveirbian <swbian@hust.edu.cn>
2018-02-13 22:56:58 +0800

    Revert "add video"
    This reverts commit ae800d021a0a21f030ebcaa99e4f960cd15aa81f.

--- source/alien_invasion.py
+++ source/alien_invasion.py
@@ -7,15 +7,11 @@ from alien import Alien
 import game_functions as gf
 from button import Button
 from scoreboard import Scoreboard
-from sound import Sound
 
 def  run_game():
     # 初始化pygame、设置和屏幕对象 
     pygame.init()
-
     ai_settings = Settings()
-    background = Sound()
-
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion ")
@@ -39,14 +35,12 @@ def  run_game():
 
     # 开始游戏的主循环
     while True:
-        gf.check_events(ai_settings, screen, stats, sb, play_button, background, 
-            ship, aliens, bullets)
+        gf.check_events(ai_settings, screen, stats, sb, play_button, ship, 
+            aliens, bullets)
 
         if stats.game_active:
-            if not pygame.mixer.music.get_busy():
-                background.play_background_sound()
             ship.update()
-            gf.update_bullets(ai_settings, screen, stats, sb, background, ship, 
+            gf.update_bullets(ai_settings, screen, stats, sb, ship, 
                 aliens, bullets)
             gf.update_aliens(ai_settings, stats, screen, sb, ship, aliens, 
                 bullets)
